3a034b0e109791dd4f5abe788dd946ec74e7a46a
acadsharsh <acads.harsh@gmail.com>
2026-02-10 08:43:27 +0530

    Avoid JSX intrinsic typing for dotlottie

--- src/components/LottieLoader.tsx
+++ src/components/LottieLoader.tsx
@@ -10,10 +10,12 @@ type LottieLoaderProps = {
 
 const DEFAULT_SRC = "https://lottie.host/dbd6d563-6819-4774-81a1-bf5165fcccf5/8hOCZo4o8j.lottie";
 
+const DotLottie = "dotlottie-wc" as unknown as React.ElementType;
+
 export default function LottieLoader({ message, size = 220, className }: LottieLoaderProps) {
   return (
     <div className={`flex flex-col items-center gap-3 ${className ?? ""}`}>
-      <dotlottie-wc
+      <DotLottie
         src={DEFAULT_SRC}
         style={{ width: size, height: size }}
         autoplay
