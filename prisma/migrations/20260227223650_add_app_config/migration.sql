e92c43dd19e6b5c8f13f5eebdebfede4274e3d42
acadsharsh <acads.harsh@gmail.com>
2026-02-27 22:38:01 +0530

    Add AppConfig migration

---
+++ prisma/migrations/20260227223650_add_app_config/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "AppConfig" (
+    "key" TEXT NOT NULL,
+    "maintenanceMode" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "AppConfig_pkey" PRIMARY KEY ("key")
+);
+
